070538a1b83b89319a9d37225a6711e6d85bb36f
maddymaster <maddymaster@gmail.com>
2016-11-27 01:21:44 +0530

    Final Commit

--- example-args.js
+++ example-args.js
@@ -1,5 +1,27 @@
-var argv = require('yargs').argv;
+var argv = require('yargs')
+	.command('hello', 'Greets the user', function (yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Your first name goes here',
+				type: 'string'
+			},
+			lastname: {
+				demand: true,
+				alias: 'l',
+				description: 'Your last name goes here',
+				type:'string'
+			}
+		}).help('help');
+	})
+	.command('get', 'some description', function(yargs){
+
+	})
+	.help('help')
+	.argv;
 var command = argv._[0];
+
 console.log(argv);
 
 if (command === 'hello' && typeof argv.name !== 'undefined' && typeof argv.lastname !== 'undefined') {
